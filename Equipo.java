78abaccce69f86e2ebb3ce67cdf583124d33f0ae
Josuecastro105 <jacoxd504@gmail.com>
2026-03-07 21:23:06 -0600

    Add files via upload

---
+++ Equipo.java
@@ -0,0 +1,32 @@
+package modelo;
+
+public class Equipo {
+
+    private String fabricante;
+    private String modelo;
+    private String microprocesador;
+
+    public Equipo(String fabricante, String modelo, String microprocesador) {
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+        this.microprocesador = microprocesador;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getMicroprocesador() {
+        return microprocesador;
+    }
+
+    public String mostrarDatos() {
+        return "Fabricante: " + fabricante +
+               "\nModelo: " + modelo +
+               "\nMicroprocesador: " + microprocesador;
+    }
+}
